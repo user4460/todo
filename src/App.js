5ad8cc764a5e600e4ac3f23366a28ec7c5e06c32
user <user446044460@gmail.com>
2022-07-18 18:28:45 +0800

    useState hanle　を作成

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-//reactをインポートする
-import logo from './logo.svg';
+//react,useStateをインポートする
+import React from 'react';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
   //useStateを使って、stateを定義する
+  //todoListとtodoの一つで分ける?
+  const [todoList, setTodoList] = useState();
+  const [todo, setTodo] = useState();
 
   //handleOnCreateを使って、todoを追加する
+  //stateを使って、todoを追加する
+  handleOnCreate = () => {
+    this.setTodo = todo;
+  }
 
-  //stateを使って、todoを
-
-  //handleOnChangeを使って、todoを変更する
-
-  //handleOnCheckを使って、todoを完了する
+  //handleOnChangeを使って、todoを変更する→今はしなくていい
+  //handleOnCheckを使って、todoを完了する→今はしなくていい
 
   //handleOnRemoveを使って、todoを削除する
+  handleOnRemove = () => {
+
+  }
 
   return (
     <div className="App">
       {/*　formを作成する*/} 
       <form >
-
+        
       {/*　inputでテキスト入力を作成する*/} 
       {/*　inputでtodo作成ボタンを作成する */} 
       
@@ -28,7 +36,7 @@ function App() {
       {/*　削除ボタンを作る　*/} 
      
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
