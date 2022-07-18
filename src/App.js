d00b4017a9b6c9cabc07e35d7f93fc906ebf08db
user <user446044460@gmail.com>
2022-07-18 19:18:37 +0800

    onClick function

--- src/App.js
+++ src/App.js
@@ -11,47 +11,50 @@ function App() {
 
   //handleOnCreateを使って、todoを追加する
   //stateを使って、todoを追加する
-  
-  const handleOnCreate = () => {
 
+  /*const handleCreate = (todo) => {
 
     this.setTodo = todo;
     setTodoList([...todoList, todo]);
     this.setState = todo;
   }
+  */
 
-  //handleOnChangeを使って、todoを変更する→今はしなくていい
-  //handleOnCheckを使って、todoを完了する→今はしなくていい
+  //handleChangeを使って、todoを変更する→今はしなくていい
+  //handleCheckを使って、todoを完了する→今はしなくていい
 
-  //handleOnRemoveを使って、todoを削除する
-  const handleOnDelete = () => {
+  //handleDeleteを使って、todoを削除する
+  /*
+  const handleDelete = () => {
 
     const filter = todoList.filter(todo => todo.id !== id);
   }
+  */
 
   return (
     <div className="App">
 
-      {/*　formを作成する*/} 
+      {/*　formを作成する*/}
       <form >
-      {/*　inputでテキスト入力を作成する*/} 
-      {/*　inputでtodo作成ボタンを作成する */} 
-      <input type="text" />
-      <button type="submit" onClick={handleOnCreate} >作成</button>
+        {/*　inputでテキスト入力を作成する*/}
+        {/*　inputでtodo作成ボタンを作成する */}
+        <input type="text" />
+        <button type="submit" onClick={(todo) => this.setTodo()} >作成</button>
       </form>
 
-       {/*　作成したtodoをmapリストで表示する　*/}
+      {/*　作成したtodoをmapリストで表示する　*/}
       {/* 
        {todoList.map((todo) => (
           return (
-            <ul>
-            <li>{todo}</li>
-            </ul>  
+            <li key={id}>
+            {todo.text}
+            </li>
+              
         )
       } 
       */}
-      {/*　削除ボタンを作る　*/} 
-      <button type="submit" onClick={handleOnDelete}>削除</button>
+      {/*　削除ボタンを作る　*/}
+      <button type="submit" onClick={() => { }}>削除</button>
 
     </div>
   );
