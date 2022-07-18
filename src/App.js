a6fcf503bddfe0b4869dfb4d414babe138dcb09d
user <user446044460@gmail.com>
2022-07-18 18:18:45 +0800

    App.jsにコメント追加

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
+//reactをインポートする
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  //useStateを使って、stateを定義する
 
-  const [todo, setTodo] = useState();
-  
+  //handleOnCreateを使って、todoを追加する
+
+  //stateを使って、todoを
+
+  //handleOnChangeを使って、todoを変更する
+
+  //handleOnCheckを使って、todoを完了する
+
+  //handleOnRemoveを使って、todoを削除する
+
   return (
     <div className="App">
+      {/*　formを作成する*/} 
+      <form >
+
+      {/*　inputでテキスト入力を作成する*/} 
+      {/*　inputでtodo作成ボタンを作成する */} 
+      
+      
+      {/*　todoをmapで表示する　*/} 
+      {/*　削除ボタンを作る　*/} 
+     
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,9 +38,10 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React now
+          Learn React
         </a>
       </header>
+      </form>
     </div>
   );
 }
