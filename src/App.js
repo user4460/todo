847ae06b59126f55017a4f57437c8396fc7e707b
user <user446044460@gmail.com>
2022-07-20 12:42:44 +0800

    array push filter

--- src/App.js
+++ src/App.js
@@ -18,24 +18,26 @@ function App() {
 ・初期値を入れておく"	初期値は全パターン網羅できるように定義しておくと便利  useState */}
   const [todos, setTodos] = useState();
   const [todo, setTodo] = useState();
-
+  console.log(todo);
+  console.log(todos);
   //handleOnCreateを使って、todoを追加する
   //stateを使って、todoを追加する
-
-  /*const handleCreate = (todo) => {
-
-    this.setTodo = todo;
-    array.push()
-    this.setState = todo;
-  }
-  */
-
-  //handleDeleteを使って、todoを削除する
   /*
-  const handleDelete = () => {
-
-    const filter = todos.filter(todo => todo.id !== id);
-  }
+    const handleCreate = (todo) => {
+  
+      this.setTodo = todo;
+      array.push()
+    }
+    
+    console.log(array)
+    //handleDeleteを使って、todoを削除する
+    
+    const handleDelete = () => {
+  
+      const filter = todos.filter(todo => todo.id !== id);
+    }
+    
+    console.log(filter)
   */
 
   return (
@@ -48,34 +50,31 @@ function App() {
         {/*array.push()もしくはconst newArray = [...array, newObj] 
         "onClick State,setState" */}
         <input type="text" value={todo} />
-        <button type="submit" onClick={(todo) => setTodo(todo)} >作成</button>
+        <button type="submit" onClick= {(todo) => { array.push(todo)}} >作成</button>
       </form>
 
       {/*"リスト形式で以下の項目を作る
         ・進捗状態・編集ボタン・削除ボタン"	"ul,li button"*/}
       {/*　作成したtodoをmapリストで表示する　*/}
       {/* liをループさせる
-       {todos.map((todo) => (
-        return (
-        for (let i = 0; i < todos.length; i++) {
+       {todos.map((todo) => {
             <ul>
-            <li key={id}>
-            {todos[i]}
-            </li>
+            <li key={todo.id}>{todo}</li>
             </ul>
-        )
+        )}
       } 
       */}
-      {/*　削除ボタンを作る　"引数　array.filter()もしくはarray.splice()"　*/}
 
-      {/*"・特定のTODOを削除するメソッドを定義する。
+      {/*"削除ボタンを作る
+      ・特定のTODOを削除するメソッドを定義する。
 　        ・引数にはTodoのidもしくはindexを定義する
 　        ・渡されたTodoのidもしくはindexを使ってtodos配列内の特定のTodoを削除する
          ・削除ボタンを押したときに削除メソッドが実行されるようにする。
 　        ・引数にTodoのidもしくはindexを渡して特定のTodoが削除できるようにする"	"引数
             array.filter()もしくはarray.splice() */}
-      "
-      <button type="submit" onClick={() => { }}>削除</button>
+
+      <button type="submit" onClick={(id) => { array.splice(id);
+       }}>削除</button>
 
     </div>
   );
