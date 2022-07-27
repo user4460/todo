4b6709ccbf58a5efa44105d0e47dce4eeb6098b6
yoshiaki.n <52188314+user4460@users.noreply.github.com>
2022-07-27 14:43:58 +0800

    p

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
   {/* コンポーネント内に ・todosステートを定義する・初期値を入れておく
 	初期値は全パターン網羅できるように定義しておくと便利  useState */}
 
-  const [todos, setTodos] = useState([]);
   const [todo, setTodo] = useState('');
+  const [todos, setTodos] = useState([]);
   const [id, setId] = useState(); 
 
   //handleCreateを使って、todoを作成する 
@@ -40,11 +40,14 @@ function App() {
       <form >
         {/*　inputでtodo入力を作成する,buttonでtodo作成ボタンを作成する */}
         <input type="text" value={todo} onChange={handleCreate} />
-        <button type="submit" onClick={handleAdd} >作成</button>
+        <button type="submit" value="作成" onClick={handleAdd} >作成</button>
       </form>
 
       {/*"リスト形式で以下の項目を作る ・進捗状態・編集ボタン・削除ボタン"	"ul,li button"*/}
       {/*　作成したtodoをmapリストで表示する */}
+      
+      <p>{todo}</p>
+      
       {todos.map(() => {
         <ul>
           <li key={todo.id}>{todo}</li>
